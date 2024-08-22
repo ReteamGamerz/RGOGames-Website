c1b490f639b8e5ffbf7ef8190731d440c148f47e
ReteamGamerz <76695753+ReteamGamerz@users.noreply.github.com>
2024-08-22 20:48:32 +0530

    Add files via upload

---
+++ classtime.js
@@ -0,0 +1,11 @@
+function redirect_install() {
+    window.location = "install.html";
+}
+
+function redirect_home() {
+    window.location = "index.html";
+}
+
+function redirectToApp(){
+    window.open("");
+}
